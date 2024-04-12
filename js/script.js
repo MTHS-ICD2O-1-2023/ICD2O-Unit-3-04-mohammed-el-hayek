49d3f3453fe610fdf1dd909a33cf33671e9601b6
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-04-12 04:10:38 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed EL-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the volume of a pyramid
+ */
+function calculate() {
+  // input
+  const f = parseInt(document.getElementById('Fahrenheit').value)
+
+  // process
+  const c = (f - 32) * 5 / 9
+
+  // output
+  document.getElementById('Celsius').innerHTML = `The Temperature is: ${c.toFixed(2)} °C`
+}
